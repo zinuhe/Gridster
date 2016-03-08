ad2d62db5bbb0d8ff79778b36e4524eef3fb82f6
jimmy.saavedra <jimmy.saavedra@statpro.com>
2016-03-08 18:24:22 -0500

    Added

--- Gridster/javascript/gridder.js
+++ Gridster/javascript/gridder.js
@@ -4,7 +4,8 @@ $(function () { //DOM Ready
 
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140]
+        widget_base_dimensions: [140, 140],
+        resize: { enabled: true }
     });
 
 });
